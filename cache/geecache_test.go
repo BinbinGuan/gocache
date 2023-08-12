9fbb52a3971c983f96855af956686df1f3f5d073
binbin.guan <983529954@qq.com>
2023-08-12 22:48:38 +0800

    同步

---
+++ cache/geecache_test.go
@@ -0,0 +1,69 @@
+package cache
+
+import (
+	"fmt"
+	"log"
+	"reflect"
+	"testing"
+)
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func TestGetter(t *testing.T) {
+	var f Getter = GetterFunc(func(key string) ([]byte, error) {
+		return []byte(key), nil
+	})
+
+	expect := []byte("key")
+	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
+		t.Fatal("callback failed")
+	}
+}
+
+// 2<<10		2*2^10=	2*1024=	2048
+func TestGet(t *testing.T) {
+	loadCounts := make(map[string]int, len(db))
+	gee := NewGroup("scores", 2<<10, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				if _, ok := loadCounts[key]; !ok { //	如果loadCounts中没有key，就初始化为0
+					loadCounts[key] = 0
+				}
+				loadCounts[key]++ //	每次调用Get()，就会将loadCounts[key]的值加1
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	for k, v := range db {
+		// 		如果缓存中不存在key，就调用回调函数，从回调函数中会将db中的值存入缓存
+		if view, err := gee.Get(k); err != nil || view.String() != v {
+			t.Fatal("failed to get value of Tom")
+		}
+		if _, err := gee.Get(k); err != nil || loadCounts[k] > 1 { //	如果缓存中存在key，就不会再调用回调函数
+			t.Fatalf("cache %s miss", k)
+		}
+	}
+
+	if view, err := gee.Get("unknown"); err == nil {
+		t.Fatalf("the value of unknow should be empty, but %s got", view)
+	}
+}
+
+func TestGetGroup(t *testing.T) {
+	groupName := "scores"
+	NewGroup(groupName, 2<<10, GetterFunc(
+		func(key string) (bytes []byte, err error) { return }))
+	if group := GetGroup(groupName); group == nil || group.name != groupName {
+		t.Fatalf("group %s not exist", groupName)
+	}
+
+	if group := GetGroup(groupName + "111"); group != nil {
+		t.Fatalf("expect nil, but %s got", group.name)
+	}
+}
